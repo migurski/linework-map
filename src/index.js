64482c31c7544bd81494ab1178831238d9c665be
Michal Migurski <mike@teczno.com>
2017-08-12 11:24:36 -0700

    Added easing transitions and historic Stamen colors

--- src/index.js
+++ src/index.js
@@ -3,53 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {states} from './states.js';
 
-class State extends React.Component
+// Colors:
+// http://chromatron.s3-website-us-east-1.amazonaws.com/#eNpNzssKwjAQBdB/uW6ziHknvyIuNGklGFqoCErJvxvyoM5mNnPm3h1+Tev2grvsiAGOEswxJTicqJFMTyD4wDEtCL5lK0uwwRmaSQPnP6C4VKoDJSvgVFUghR2CHYIxORs/IliPaMLwAfgByjtrdQNc9E62AU3zleB+88/Htr6XUCvVKfdxCVMxIv8ABZY9CA==
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+function ease(t, b, c, d)
 {
-    constructor(props)
-    {
-        super(props)
+    t=t/d-1;
+    return c*((t)*t*t + 1) + b;
+}
 
-        var geoms, path = [];
+function get_geometry_path(geometry)
+{
+    var geoms, path = [];
+
+    if(geometry.type === 'MultiPolygon') {
+        geoms = geometry.coordinates;
+    } else if(geometry.type === 'Polygon') {
+        geoms = [geometry.coordinates];
+    }
+
+    for(var part, i = 0; i < geoms.length; i++)
+    {
+        part = geoms[i];
     
-        if(props.geometry.type === 'MultiPolygon') {
-            geoms = props.geometry.coordinates;
-        } else if(props.geometry.type === 'Polygon') {
-            geoms = [props.geometry.coordinates];
-        }
-    
-        for(var part, i = 0; i < geoms.length; i++)
+        for(var ring, j = 0; j < part.length; j++)
         {
-            part = geoms[i];
-        
-            for(var ring, j = 0; j < part.length; j++)
-            {
-                ring = part[j];
-                path = path.concat(['M', ring[0][0], ring[0][1]]);
+            ring = part[j];
+            path = path.concat(['M', ring[0][0], ring[0][1]]);
 
-                for(var coord, k = 1; k < ring.length; k++)
-                {
-                    coord = ring[k];
-                    path = path.concat(['L', coord[0], coord[1]]);
-                }
-            
-                path.push('Z');
+            for(var coord, k = 1; k < ring.length; k++)
+            {
+                coord = ring[k];
+                path = path.concat(['L', coord[0], coord[1]]);
             }
-        }
         
-        this.path = path.join(' ');
+            path.push('Z');
+        }
     }
     
-    render()
-    {
-        var props = this.props;
-    
-        return <path d={this.path}
-            onClick={() => { props.states.selectState(props.properties.NAME) }}
-            onMouseOver={() => { props.states.hoverState(props.properties.NAME) }}
-            onMouseOut={() => { props.states.hoverState(undefined) }}
-            vectorEffect="non-scaling-stroke" fillRule='evenodd'
-            style={{cursor: 'pointer', fill: (props.hovered ? '#f09' : '#ff9'), stroke: '#f90'}} />;
-    }
+    return path.join(' ');
 }
 
 function get_matrix(width, height, bbox)
@@ -66,7 +59,28 @@ function get_matrix(width, height, bbox)
         x = width/2 - xscale * _x,
         y = height/2 - yscale * _y;
     
-    return 'matrix(' + [xscale, 0, 0, yscale, x, y].join(' ') + ')';
+    return [xscale, 0, 0, yscale, x, y];
+}
+
+class State extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+        this.path = get_geometry_path(props.geometry);
+    }
+    
+    render()
+    {
+        var props = this.props;
+    
+        return <path d={this.path}
+            onClick={() => { props.states.selectState(props.properties.ISO3166_2) }}
+            onMouseOver={() => { props.states.hoverState(props.properties.ISO3166_2) }}
+            onMouseOut={() => { props.states.hoverState(undefined) }}
+            vectorEffect="non-scaling-stroke" fillRule='evenodd'
+            style={{cursor: 'pointer', fill: (props.hovered ? '#306997' : '#08527E'), stroke: '#225f8c'}} />;
+    }
 }
 
 class States extends React.Component
@@ -78,48 +92,98 @@ class States extends React.Component
         this.state = {
             states: props.states.features,
             bbox: props.states.bbox,
-            selected: undefined,
-            hovered: undefined
+            selected: null,
+            hovered: null,
+
+            moving: false,
+            move_now: null,
+            move_start: {matrix: [], time: null},
+            move_end: {matrix: [], time: null}
             };
     }
     
-    selectState(name)
+    selectState(ISO3166_2)
     {
-        this.setState({selected: name});
+        if(this.state.moving) { return }
+        
+        var move_start = {time: (new Date()).getTime(), matrix: this.findMatrix(this.state.selected)},
+            move_end = {time: (new Date()).getTime() + 500, matrix: this.findMatrix(ISO3166_2)};
+        
+        this.setState({selected: ISO3166_2,
+            moving: true, move_now: (new Date()).getTime(),
+            move_start: move_start, move_end: move_end});
     }
 
-    hoverState(name)
+    hoverState(ISO3166_2)
     {
-        this.setState({hovered: name});
+        this.setState({hovered: ISO3166_2});
+    }
+    
+    findMatrix(ISO3166_2)
+    {
+        for(var feature, i = 0; i < this.state.states.length; i++)
+        {
+            feature = this.state.states[i];
+            if(feature.properties.ISO3166_2 === ISO3166_2)
+            {
+                return get_matrix(window.innerWidth, window.innerHeight, feature.bbox);
+            }
+        }
+        
+        return get_matrix(window.innerWidth, window.innerHeight, this.state.bbox);
+    }
+    
+    moveOn()
+    {
+        if((new Date()).getTime() < this.state.move_end.time) {
+            this.setState({moving: true, move_now: (new Date()).getTime()});
+        
+        } else {
+            this.setState({moving: false, move_now: null});
+        }
     }
 
     render()
     {
         var states = [],
-            matrix = get_matrix(window.innerWidth, window.innerHeight, this.state.bbox);
+            matrix = this.findMatrix(this.state.selected);
         
         for(var feature, i = 0; i < this.state.states.length; i++)
         {
             feature = this.state.states[i];
-            states.push(<State key={feature.properties['ISO3166_2']}
-                hovered={this.state.hovered === feature.properties.NAME}
+            states.push(<State key={feature.properties.ISO3166_2}
+                hovered={this.state.hovered === feature.properties.ISO3166_2}
                 properties={feature.properties} geometry={feature.geometry}
                 states={this} />);
-            
-            if(this.state.selected === feature.properties.NAME)
-            {
-                matrix = get_matrix(window.innerWidth, window.innerHeight, feature.bbox);
-            }
         }
         
-        return <svg xmlns="http://www.w3.org/2000/svg" style={{
+        if(this.state.moving)
+        {
+            var component = this,
+                t = this.state.move_now - this.state.move_start.time,
+                d = this.state.move_end.time - this.state.move_start.time,
+                m1 = this.state.move_start.matrix,
+                m2 = this.state.move_end.matrix;
+            
+            matrix = [
+                ease(t, m1[0], m2[0] - m1[0], d), ease(t, m1[1], m2[1] - m1[1], d),
+                ease(t, m1[2], m2[2] - m1[2], d), ease(t, m1[3], m2[3] - m1[3], d),
+                ease(t, m1[4], m2[4] - m1[4], d), ease(t, m1[5], m2[5] - m1[5], d)
+                ];
+        
+            window.requestAnimationFrame(() => { component.moveOn() });
+        }
+        
+        var svg = <svg xmlns="http://www.w3.org/2000/svg" style={{
             position: 'fixed', left: 0, top: 0, width: '100%', height: '100%'}}>
             <rect onClick={() => { this.selectState(false) }} style={{
-                width: '100%', height: '100%', fill: '#fff'}} />
-            <g id="states" transform={matrix}>
+                width: '100%', height: '100%', fill: '#063566'}} />
+            <g id="states" transform={'matrix('+matrix.join(' ')+')'}>
                 {states}
             </g>
-            </svg>
+            </svg>;
+        
+        return svg;
     }
 }
 
